1828209eccd82ccf9743a74489b2b5818eb8d841
TinyDragons <minhtanpham379@gmail.com>
2024-10-25 15:10:54 +0700

    finish weight for bfs

--- Source/sokoban.py
+++ Source/sokoban.py
@@ -9,7 +9,7 @@ from astar import aStarSearch
 
 if __name__ == '__main__':
     time_start = time.time()
-    layout, method = readCommand(sys.argv[1:]).values()
+    layout, stoneWeight, method = readCommand(sys.argv[1:]).values()
     gameState = transferToGameState(layout)
     
     if method == 'astar':
@@ -17,10 +17,11 @@ if __name__ == '__main__':
     elif method == 'dfs':
         depthFirstSearch(gameState)
     elif method == 'bfs':
-        breadthFirstSearch(gameState)
+        step_cnt, node_cnt, weight_total = breadthFirstSearch(gameState, stoneWeight)
     elif method == 'ucs':
         uniformCostSearch(gameState)
     else:
         raise ValueError('Invalid method.')
     time_end=time.time()
+    print('%d steps - %d nodes - %d weights' %(step_cnt, node_cnt, weight_total))
     print('Runtime of %s: %.2f second.' %(method, time_end-time_start))
--- Source/utils.py
+++ Source/utils.py
@@ -20,30 +20,17 @@ def readCommand(argv):
     options, _ = parser.parse_args(argv)
     args = dict()
 
-    with open('./' + options.sokobanLevels,"r") as f: 
+    with open('./' + options.sokobanLevels,"r") as f:
+        weightLine = f.readline()[0:-1]
+        stoneWeight = weightLine.split(' ')
+        stoneWeight = (int(x) for x in stoneWeight)
+        
         layout = f.readlines()
 
     args['layout'] = layout
+    args['stoneWeight'] = stoneWeight
     args['method'] = options.agentMethod
     return args
-
-class PriorityQueue:
-    """Define a PriorityQueue data structure that will be used"""
-    def  __init__(self):
-        self.Heap = []
-        self.Count = 0
-
-    def push(self, item, priority):
-        entry = (priority, self.Count, item)
-        heapq.heappush(self.Heap, entry)
-        self.Count += 1
-
-    def pop(self):
-        (_, _, item) = heapq.heappop(self.Heap)
-        return item
-
-    def isEmpty(self):
-        return len(self.Heap) == 0
 
 def transferToGameState(layout):
     """Transfer the layout of initial puzzle"""
@@ -65,66 +52,85 @@ def transferToGameState(layout):
             
     return np.array(layout)
 
+# gameState
+# [[0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+#  [1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+#  [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+#  [1 0 3 0 3 0 0 0 0 0 0 0 0 0 0 0 1]
+#  [1 4 0 2 0 0 0 0 0 0 0 0 0 0 0 4 1]
+#  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]]
+
 def PosOfPlayer(gameState):
     """Return the position of agent"""
     return tuple(np.argwhere(gameState == 2)[0]) # e.g. (2, 2)
 
-def PosOfBoxes(gameState):
+def PosOfStones(gameState, stoneWeight):
     """Return the positions of boxes"""
-    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5))) # e.g. ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+    stones = tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5))) # e.g. ((3, 2), (3, 4))
+    return tuple(a + (b,) for a, b in zip(stones, stoneWeight)) # e.g. ((3, 2, 1), (3, 4, 99))
 
 def PosOfWalls(gameState):
     """Return the positions of walls"""
-    return tuple(tuple(x) for x in np.argwhere(gameState == 1)) # e.g. like those above
+    return tuple(tuple(x) for x in np.argwhere(gameState == 1)) 
 
 def PosOfGoals(gameState):
     """Return the positions of goals"""
-    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5))) # e.g. like those above
+    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5))) 
 
-def isEndState(posBox, posGoals):
+def isEndState(posStone, posGoals):
     """Check if all boxes are on the goals (i.e. pass the game)"""
-    return sorted(posBox) == sorted(posGoals)
+    return sorted(tuple(stone[0:2] for stone in posStone)) == sorted(posGoals)
 
-def isLegalAction(action, posPlayer, posBox, posWalls):
+def isLegalAction(action, posPlayer, posStone, posWalls):
     """Check if the given action is legal"""
     xPlayer, yPlayer = posPlayer
     if action[-1].isupper(): # the move was a push
         x1, y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
     else:
         x1, y1 = xPlayer + action[0], yPlayer + action[1]
-    return (x1, y1) not in posBox + posWalls
+    return (x1, y1) not in (tuple(stone[0:2] for stone in posStone)) + posWalls
 
-def legalActions(posPlayer, posBox, posWalls):
+def legalActions(posPlayer, posStone, posWalls):
     """Return all legal actions for the agent in the current game state"""
-    allActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    allActions = [[-1,0,'u','U'],[0,1,'r','R'], [1,0,'d','D'],[0,-1,'l','L']]
     xPlayer, yPlayer = posPlayer
     legalActions = []
     for action in allActions:
         x1, y1 = xPlayer + action[0], yPlayer + action[1]
-        if (x1, y1) in posBox: # the move was a push
+        if (x1, y1) in (tuple(stone[0:2] for stone in posStone)): # the move was a push
             action.pop(2) # drop the little letter
         else:
             action.pop(3) # drop the upper letter
-        if isLegalAction(action, posPlayer, posBox, posWalls):
+        if isLegalAction(action, posPlayer, posStone, posWalls):
             legalActions.append(action)
         else: 
             continue     
     return tuple(tuple(x) for x in legalActions) # e.g. ((0, -1, 'l'), (0, 1, 'R'))
 
-def updateState(posPlayer, posBox, action):
+def updateState(posPlayer, posStone, action):
     """Return updated game state after an action is taken"""
+    
     xPlayer, yPlayer = posPlayer # the previous position of player
     newPosPlayer = [xPlayer + action[0], yPlayer + action[1]] # the current position of player
-    posBox = [list(x) for x in posBox]
-    if action[-1].isupper(): # if pushing, update the position of box
-        posBox.remove(newPosPlayer)
-        posBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
-    posBox = tuple(tuple(x) for x in posBox)
+    weightPush = 0
+    
+    posStone = [list(x) for x in posStone]
+    
+    if action[-1].isupper(): # if pushing, update the position of stone
+        curPosStone = [pos for pos in posStone if pos[0:2] == newPosPlayer]
+        posStone.remove(curPosStone[0])
+        weightPush = curPosStone[0][2]
+        posStone.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1], weightPush])
+        
+    newPosStone = tuple(tuple(x) for x in posStone)
     newPosPlayer = tuple(newPosPlayer)
-    return newPosPlayer, posBox
+    
+    return newPosPlayer, newPosStone, weightPush
 
-def isFailed(posBox, posGoals, posWalls):
+def isFailed(posStone, posGoals, posWalls):
     """This function used to observe if the state is potentially failed, then prune the search"""
+    posStone = (tuple(stone[0:2] for stone in posStone))
+
     rotatePattern = [[0,1,2,3,4,5,6,7,8],
                     [2,5,8,1,4,7,0,3,6],
                     [0,1,2,3,4,5,6,7,8][::-1],
@@ -135,30 +141,50 @@ def isFailed(posBox, posGoals, posWalls):
                     [0,3,6,1,4,7,2,5,8][::-1]]
     allPattern = rotatePattern + flipPattern
 
-    for box in posBox:
-        if box not in posGoals:
-            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
-                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
-                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+    for stone in posStone:
+        if stone not in posGoals:
+            board = [(stone[0] - 1, stone[1] - 1), (stone[0] - 1, stone[1]), (stone[0] - 1, stone[1] + 1), 
+                    (stone[0], stone[1] - 1), (stone[0], stone[1]), (stone[0], stone[1] + 1), 
+                    (stone[0] + 1, stone[1] - 1), (stone[0] + 1, stone[1]), (stone[0] + 1, stone[1] + 1)]
             for pattern in allPattern:
                 newBoard = [board[i] for i in pattern]
                 if newBoard[1] in posWalls and newBoard[5] in posWalls: return True
-                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
-                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox: return True
-                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox: return True
-                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
+                elif newBoard[1] in posStone and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posStone and newBoard[2] in posWalls and newBoard[5] in posStone: return True
+                elif newBoard[1] in posStone and newBoard[2] in posStone and newBoard[5] in posStone: return True
+                elif newBoard[1] in posStone and newBoard[6] in posStone and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
     return False
 
 def cost(actions):
     """A cost function"""
     return len([x for x in actions if x.islower()])
 
-def heuristic(posPlayer, posBox, posGoals):
+def heuristic(posPlayer, posStone, posGoals):
     """A heuristic function to calculate the overall distance between the else boxes and the else goals"""
+    posStone = (tuple(stone[0:2] for stone in posStone))
+
     distance = 0
-    completes = set(posGoals) & set(posBox)
-    sortposBox = list(set(posBox).difference(completes))
+    completes = set(posGoals) & set(posStone)
+    sortposStone = list(set(posStone).difference(completes))
     sortposGoals = list(set(posGoals).difference(completes))
-    for i in range(len(sortposBox)):
-        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+    for i in range(len(sortposStone)):
+        distance += (abs(sortposStone[i][0] - sortposGoals[i][0])) + (abs(sortposStone[i][1] - sortposGoals[i][1]))
     return distance
+
+class PriorityQueue:
+    """Define a PriorityQueue data structure that will be used"""
+    def  __init__(self):
+        self.Heap = []
+        self.Count = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.Count, item)
+        heapq.heappush(self.Heap, entry)
+        self.Count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.Heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.Heap) == 0
